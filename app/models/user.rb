aa5aff09655949e3c44f2ca9a6ba701e7e4c6ff7
amandena <manda.panda91@yahoo.com>
2018-10-19 19:45:59 -0400

    added code to user table and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :costumes
+  has_many :costume_parties, through: :costumes
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+
+  def create_username
+    username = self.email.split("@")
+    username[0]
+  end
 end

3a237045308fa189af5b74ad059d5107af416fef
amandena <manda.panda91@yahoo.com>
2018-10-23 19:51:00 -0400

    added code to costume_party table and model

--- app/models/costume_party.rb
+++ app/models/costume_party.rb
@@ -1,2 +1,10 @@
 class CostumeParty < ApplicationRecord
+  belongs_to :user
+  has_many :costumes
+
+  def best_costume #method to declare the best costume based on the scare_rating
+    self.costumes.each do |costume|
+
+    end
+  end
 end
--- db/migrate/20181019223759_create_costume_parties.rb
+++ db/migrate/20181019223759_create_costume_parties.rb
@@ -1,6 +1,9 @@
 class CreateCostumeParties < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_parties do |t|
+      t.string :name
+      t.integer :user_id
+      t.integer :costume_id
 
       t.timestamps
     end

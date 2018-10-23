f80bb359fb6f6ca265189d6f5770840b1a84a2ef
amandena <manda.panda91@yahoo.com>
2018-10-22 22:58:02 -0400

    added code to costume table and model

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,12 @@
 class Costume < ApplicationRecord
+  has_many :users
+  has_many :costume_parties, through: :users
+
+  def scary?
+    if self.scare_rating > 5
+      "OH MY GHOST!"
+    else
+      "Better luck next year, kid."
+    end
+  end
 end
--- db/migrate/20181019223655_create_costumes.rb
+++ db/migrate/20181019223655_create_costumes.rb
@@ -1,6 +1,13 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
+      t.string :name
+      t.string :clothing_1
+      t.string :clothing_2
+      t.string :accessory_1
+      t.string :accessory_2
+      t.integer :cost
+      t.integer :scare_rating
 
       t.timestamps
     end

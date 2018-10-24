e4de8df79eb9aa9ddef30728d9733b23f6ec450a
amandena <manda.panda91@yahoo.com>
2018-10-24 19:20:48 -0400

    fixed typo in costume_parties table

--- db/migrate/20181019223759_create_costume_parties.rb
+++ db/migrate/20181019223759_create_costume_parties.rb
@@ -2,7 +2,7 @@ class CreateCostumeParties < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_parties do |t|
       t.string :name
-      t.date : :date
+      t.date :date
       t.integer :time
       t.integer :user_id
 

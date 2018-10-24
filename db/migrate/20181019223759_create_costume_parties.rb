ccdcaf942eefb072c2a3738a329c1d5db7e51446
amandena <manda.panda91@yahoo.com>
2018-10-24 19:13:23 -0400

    refactored costume_parties table

--- db/migrate/20181019223759_create_costume_parties.rb
+++ db/migrate/20181019223759_create_costume_parties.rb
@@ -2,6 +2,8 @@ class CreateCostumeParties < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_parties do |t|
       t.string :name
+      t.date : :date
+      t.integer :time
       t.integer :user_id
 
       t.timestamps

78e776673aed27dc0b1a174378de910f249e247e
amandena <manda.panda91@yahoo.com>
2018-10-30 19:06:14 -0400

    added methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?, :require_login
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id] if session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to root_path unless logged_in?
+  end
 end

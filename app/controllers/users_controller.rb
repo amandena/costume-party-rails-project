fe98d85a6bf2d80aee2d82b96c732fd6b37a031c
amandena <manda.panda91@yahoo.com>
2018-11-01 13:50:40 -0400

    stubbed out user controller actions and private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end

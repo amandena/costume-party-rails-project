1f55e7c4dcf1c1911adfe28c58fb92d9e0db4669
amandena <manda.panda91@yahoo.com>
2018-11-01 13:55:41 -0400

    added code to user new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,17 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update]
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show

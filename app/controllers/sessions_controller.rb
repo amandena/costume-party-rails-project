b5b7ba1a8146e49d38e1085bcc2dfa39808625ab
amandena <manda.panda91@yahoo.com>
2018-11-05 21:38:23 -0500

    added session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    @users = User.all
+  end
 
+  def create
+    @user = User.find(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end

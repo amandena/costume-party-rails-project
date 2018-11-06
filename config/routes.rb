8ccb6cb4ede38d40aecf557450260cb6941195cc
amandena <manda.panda91@yahoo.com>
2018-11-05 22:25:28 -0500

    added home page route, action, and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :costume_parties
   resources :costumes
   resources :users
+
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

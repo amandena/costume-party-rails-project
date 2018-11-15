2319fddcef07312cab9376cea105a7e7474cf7c2
amandena <manda.panda91@yahoo.com>
2018-11-14 19:59:18 -0500

    stubbed out costume controller actions and costume_params method

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,2 +1,28 @@
 class CostumesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :clothing_1, :clothing_2, :accessory_1, :accessory_2, :cost, :scare_rating, :costume_party_id)
+  end
 end

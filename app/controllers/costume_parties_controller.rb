c2090bbba382ce0aaf1550651012a763a4779b12
amandena <manda.panda91@yahoo.com>
2018-11-24 23:18:22 -0500

    stubbed out costume parties actions and strong params

--- app/controllers/costume_parties_controller.rb
+++ app/controllers/costume_parties_controller.rb
@@ -1,2 +1,28 @@
 class CostumePartiesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def costume_party_params
+    params.require(:costume_party).permit(:name, :date, :time, :user_id)
+  end
 end

a5105bf9d40d3c77b5292fc183b41c6c82585481
amandena <manda.panda91@yahoo.com>
2018-11-25 19:37:39 -0500

    fixed sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user][:email])
+    #binding.pry
+    @user = User.find_by(email: params[:user][:email])
+
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to @user

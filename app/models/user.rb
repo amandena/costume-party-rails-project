e1ee52f258d3e7e268e6e96893de24d03643ade3
amandena <manda.panda91@yahoo.com>
2018-12-02 17:56:26 -0500

    added error messages to user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :costumes
   has_many :costume_parties, through: :costumes
 
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates :password, presence: true
+  validates :email, presence: { message: "Required field"}
+  validates :email, uniqueness: { message: "Email already in use, please use another email or sign in"}
+  validates :password, presence: { message: "Required field"}
 
   def create_username
     username = self.email.split("@")

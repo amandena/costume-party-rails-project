8d5590c7b7e24011c080693be99798f5665425f1
amandena <manda.panda91@yahoo.com>
2018-12-03 21:25:49 -0500

    fixed link br's on user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
   def create
     #binding.pry
     @user = User.find_by(email: params[:user][:email])
-
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to @user

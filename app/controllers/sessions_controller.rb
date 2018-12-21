02bad6623a5e0988a3b0c5efdc3065611b846de6
amandena <manda.panda91@yahoo.com>
2018-12-20 23:01:29 -0500

    tried fixing sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,14 @@ class SessionsController < ApplicationController
 
   def create
     #binding.pry
-    if @user = User.from_omniauth(uid: params[:uid])
+    if @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        render 'new'
+      end
+    elsif @user = User.from_omniauth(env["omniauth.auth"])
       session[:user_id] = @user.id
       redirect_to @user
     else

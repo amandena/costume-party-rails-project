dd41f527508dfb6cb505f92830059a720400ef85
amandena <manda.panda91@yahoo.com>
2019-01-23 21:49:14 -0500

    reconfigured sessions create action and omniauth works again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,18 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
     @users = User.all
-    #binding.pry
   end
 
   def create
-    #binding.pry
-    if params[:user] == nil || params[:user].empty?
-      @user = User.new
-      render 'new'
-    elsif params[:user]
+    if auth
+      @user = User.find_or_create_by(uid: auth[:uid]) do |user|
+        user.email = auth[:info][:email]
+        user.password = SecureRandom.hex
+      end
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
       @user = User.find_by(email: params[:user][:email])
-      #binding.pry
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to @user
@@ -21,16 +22,6 @@ class SessionsController < ApplicationController
         flash[:error] = "*You must log in with an email and password.*"
         render 'new'
       end
-    else
-      #binding.pry
-      @user = User.find_or_create_by(uid: auth[:uid]) do |user|
-        #binding.pry
-        user.email = auth[:info][:email]
-        user.password = SecureRandom.hex
-      end
-      #binding.pry
-      session[:user_id] = @user.id
-      redirect_to @user
     end
   end
 

a16b4ae79e5b10ff6cb3adbc186153789005fcce
amandena <manda.panda91@yahoo.com>
2019-01-25 20:18:41 -0500

    added validations and error messages to costume new view and model

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,6 +2,7 @@ class Costume < ApplicationRecord
   belongs_to :user
   belongs_to :costume_party
 
+  validates :name, :clothing_1, :clothing_2, :accessory_1, :accessory_2, :cost, :scare_rating, :costume_party_id, presence: true
   validates :scare_rating, inclusion: {in: 1..10, message: "must be a number between 1-10"}
 
   def self.best_quality?
